a13b7e164cb48dd85c3de9680c5df4d0b73757c6
ayamazaki <akira.yamazaki0102@gmail.com>
2023-11-12 08:26:47 +0000

    Update 6 files

--- src/index.mjs
+++ src/index.mjs
@@ -8,3 +8,24 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  document.getElementById("incomplete-list").appendChild(div);
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
